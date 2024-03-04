c8bfb800831b5f547d45ef55777c30bec1b65639
Ido <Ido>
2024-03-04 21:46:54 +0200

    add search page

---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,54 @@
+import { Button, TextField } from "@mui/material";
+import { useState } from "react";
+import useSWR from "swr";
+import { WordList } from "../components/WordsList";
+import styled from "styled-components";
+
+export function SearchPage() {
+  const [searchValue, setSearchValue] = useState("");
+  const [url, setUrl] = useState("");
+
+  const fetcher = (url: string) =>
+    fetch(url).then((res) => {
+      return res.json();
+    });
+  const { data, error, isLoading } = useSWR(url, fetcher, {
+    refreshInterval: 1000,
+  });
+
+  function search() {
+    setUrl(`http://localhost:8080/api/v1/similar?word=${searchValue}`);
+  }
+
+  return (
+    <>
+      <SearchContainer>
+        <TextField
+          style={{ marginRight: "2rem" }}
+          id="outlined-basic"
+          label="Search word"
+          variant="outlined"
+          value={searchValue}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setSearchValue(event.target.value);
+          }}
+        />
+        <Button onClick={search} variant="contained">
+          Search
+        </Button>
+      </SearchContainer>
+
+      <div>
+        {isLoading && <div>Loading data...</div>}
+        {error && <div>Error fetching data: {data}</div>}
+        {data && <WordList similarWords={data.similar} />}
+      </div>
+    </>
+  );
+}
+
+const SearchContainer = styled.div`
+  display: flex;
+  justify-content: space-center;
+  margin: 2rem;
+`;
